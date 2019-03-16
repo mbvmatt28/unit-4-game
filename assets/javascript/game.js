92b5a559012bf68516d57cf75a54fcb5e5973ceb
Matthew Vanley <matt@vibrantsitedesign.com>
2019-03-16 17:13:06 -0500

    working!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,11 +6,19 @@ var wins = 0;
 
 //Create a start game button which will initialize everything
 var startGame = function (){
+    $(".crystals").empty();
+//Crystal Images
+var images = [
+    "https://www.bespoke-gems.com/images/gemstones/photo-gallery/1/doug-menadue-bespoke-gems-Finished%20this%20beauty%20last%20week.%20%20This%20is%2029.32%20carats%20of%20golden%20honey%20citrine%20cu.jpg",
+    "http://www.chicmags.com/wp-content/uploads/2013/01/Sapphires.jpg",
+    "http://www.artiniangems.com/wp-content/uploads/red_gem_OurStory-192x192.png",
+    "http://www.store.palagems.com/Images/gem_news/featured_neon_tourm.jpg"
+]
 //Generate random number between 30 and 70
     winningNum = Math.floor(Math.random() * 40 ) + 30;
 
-    document.write('Winning Number: ' + winningNum);
-
+    $("#result").html("Winning Number: " + winningNum);
+    
     //Generate 4 individual random numbers from 1 to 9
     for(var i = 0; i < 4; i++){
         var random = Math.floor(Math.random() * 8) + 1;
@@ -19,6 +27,10 @@ var startGame = function (){
                 "class": 'crystal',
                 "data-random": random
             });
+            crystal.css({
+                "background-image":"url('" + images[i] + "')",
+                "background-size":"cover"
+            })
         $(".crystals").append(crystal);
         console.log(random)
     }
@@ -28,20 +40,24 @@ var startGame = function (){
 startGame();
 
 //Add click function to crystals which will add that number to your currentScore
-$(".crystal").on('click', function(){
+$(document).on('click', ".crystal", function () {
     var num = parseInt($(this).attr('data-random'));
     currentScore += num;
     console.log(currentScore)
+    $("#current").html("Current Score: " + currentScore);
     if(currentScore > winningNum){
-        lost--;
-        $("#losses").html(losses);
+        losses++;
+        $("#losses").html("Losses: " + losses);
+        currentScore = 0;
         startGame();
     }
     else if(currentScore === winningNum){
         wins++;
-        $("#wins").html(wins);
+        $("#wins").html("Wins: " + wins);
+        currentScore = 0;
         startGame();
     }
+    
 })
 //If total score === winningNum then alert("you win!")
 
