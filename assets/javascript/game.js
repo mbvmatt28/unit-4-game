cd2e3d9ffae7d017a270506d0d1f8d4489e1e6d8
Matthew Vanley <matt@vibrantsitedesign.com>
2019-03-16 09:37:07 -0500

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,38 +1,47 @@
 //Variables
 var winningNum
-var currentScore
-var c1
-var c2
-var c3
-var c4
+var currentScore = 0;
+var losses = 0;
+var wins = 0;
 
 //Create a start game button which will initialize everything
-
+var startGame = function (){
 //Generate random number between 30 and 70
-winningNum = Math.floor(Math.random() * 40 ) + 30;
+    winningNum = Math.floor(Math.random() * 40 ) + 30;
 
+    document.write('Winning Number: ' + winningNum);
 
+    //Generate 4 individual random numbers from 1 to 9
+    for(var i = 0; i < 4; i++){
+        var random = Math.floor(Math.random() * 8) + 1;
+        var crystal = $("<div>");
+            crystal.attr({
+                "class": 'crystal',
+                "data-random": random
+            });
+        $(".crystals").append(crystal);
+        console.log(random)
+    }
 
-document.write('Winning Number: ' + winningNum);
+    }
 
-
-
-//Generate 4 individual random numbers from 1 to 9
-for(var i = 0; i < 4; i++){
-    var random = Math.floor(Math.random() * 8) + 1;
-    var crystals = $("<div>");
-        crystals.attr({
-            "class": 'crystals',
-            "data-random": random
-        });
-    $(".crystals").append(crystals);
-    console.log(random)
-}
-
+startGame();
 
 //Add click function to crystals which will add that number to your currentScore
-$(".crystals").on('click', function(){
-    console.log($(this).attr('data-random'));
+$(".crystal").on('click', function(){
+    var num = parseInt($(this).attr('data-random'));
+    currentScore += num;
+    console.log(currentScore)
+    if(currentScore > winningNum){
+        lost--;
+        $("#losses").html(losses);
+        startGame();
+    }
+    else if(currentScore === winningNum){
+        wins++;
+        $("#wins").html(wins);
+        startGame();
+    }
 })
 //If total score === winningNum then alert("you win!")
 
